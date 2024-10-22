49bc43a2e1ec2927527489fe815cfda3a4460093
Lokhy87 <a.bolta@gmail.com>
2024-10-22 20:21:35 +0200

    AP Programacion

---
+++ PROGRAMACION/AP41/3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DNI</title>
+</head>
+<body>
+    <h1>Crea un programa que calcule la letra de un DNI en base al número pasado. El cálculo resulta de obtener el resto de la división del número entre 23, 
+        y con este número, seleccionar la letra de una lista por posición, es decir, si el número es 14, entonces la letra 14 , dado que se corresponde 
+        con la posición 14 de la secuencia TRWAGMYFPDXBNJZSQVHLCKE.</h1>
+
+    <form method="POST">
+        <label for="numero1">Introduce el primer número entero:</label>
+        <input id="numero1" name="numeros[]" type="number"><br>
+               
+        <input type="submit" value="Enviar">
+    </form>
+
+    <?php
+  
+
+  
+    ?>
+</body>
+</html>
