219b81e44fcb365dab411b569fbe17af8841ef38
Lokhy87 <a.bolta@gmail.com>
2024-10-22 20:21:35 +0200

    AP Programacion

---
+++ PROGRAMACION/AP41/4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LANZAMIENTO DADOS</title>
+</head>
+<body>
+    <h1>Escribir un programa que simule el lanzamiento de dos dados. Haz uso de la función random_int() para obtener números aleatorios entre 1 y 6 para 
+        cada uno de los lanzamientos de los dos dados. Esto se repite por cada jugador, siendo 2 el número de jugadores y 5 tiradas cada uno. 
+        Almacena en un array para cada tirada que jugador gana, o si empatan
+        Con el array generado de victorias y empates, calcula los porcentajes de partidas ganadas por el jugador 1, por el jugador 2 y los empates</h1>
+
+    <?php
+  
+
+  
+    ?>
+</body>
+</html>
