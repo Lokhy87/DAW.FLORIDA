e26de1956024b73e77d4d71b6cc91fd23fd81f37
Lokhy87 <a.bolta@gmail.com>
2024-10-24 20:20:55 +0200

    segumos con AP41 programaiocn

--- PROGRAMACION/AP41/1.php
+++ PROGRAMACION/AP41/1.php
@@ -10,24 +10,23 @@
     <form method="POST">
         <label for="numero1">Introduce el primer número entero:</label>
         <input id="numero1" name="numeros[]" type="number"><br>
-        
+        <br>
         <label for="numero2">Introduce el segundo número entero:</label>
         <input id="numero2" name="numeros[]" type="number"><br>
-        
+        <br>
         <label for="numero3">Introduce el tercer número entero:</label>
         <input id="numero3" name="numeros[]" type="number"><br>
-        
+        <br>
         <label for="numero4">Introduce el cuarto número entero:</label>
         <input id="numero4" name="numeros[]" type="number"><br>
-        
+        <br>
         <label for="numero5">Introduce el quinto número entero:</label>
         <input id="numero5" name="numeros[]" type="number"><br>
-        
+        <br>
         <input type="submit" value="Enviar">
     </form>
 
     <?php
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         $numeros = $_POST['numeros'];
