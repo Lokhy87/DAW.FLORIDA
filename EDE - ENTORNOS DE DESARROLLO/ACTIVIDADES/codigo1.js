342a1abbb3beb5ca709d8d375419dc812f209811
Lokhy87 <a.bolta@gmail.com>
2024-12-13 19:44:33 +0100

    Unidades EDE

---
+++ EDE - ENTORNOS DE DESARROLLO/ACTIVIDADES/codigo1.js
@@ -0,0 +1,16 @@
+/* SUMA DE LOS VALORES DE UN ARRAY */ 
+
+let numeros=[1,2,3,4];
+let suma=0;
+let longitud = numeros.length;
+
+for (let i=0;i<=longitud;i++){
+    suma+=numeros[i];
+}
+
+/*
+SÓLO PUEDES USAR console.log(); PARA PROBAR EL CÓDIGO
+Y SÓLO TAL Y COMO ESTÁ ESCRITO
+*/
+
+console.log(suma);
