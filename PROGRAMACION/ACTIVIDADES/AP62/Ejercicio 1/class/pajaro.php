34cbccef35defc6c30a98d413170989bc59b1415
Lokhy87 <a.bolta@gmail.com>
2025-02-15 16:45:34 +0100

    Cambio Programacion

--- PROGRAMACION/ACTIVIDADES/AP62/Ejercicio 1/class/pajaro.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("animal.php");
-
-class Pajaro extends Animal {
-    public function hacerSonido() {
-        echo "{$this->getNombre()} de {$this->getEdad()} años de edad, dice: Pio Pio.<br>";
-    }
-}
--- PROGRAMACION/ACTIVIDADES/AP62/Ejercicio 2/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("./class/factura_electronica.php");
-
-$factura1 = new Factura_electronica(2, "Jose", 200000, "pepe@gmail.com");
-
-var_dump($factura1);
