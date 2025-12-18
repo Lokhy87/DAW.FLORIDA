02982ae28d1f08d41ebd26961557c60c15622437
Lokhy87 <a.bolta@gmail.com>
2025-12-18 15:50:45 +0100

    sostenibilidad

--- SOSTENIBILIDAD APLICADA AL SISTEMA PRODUCTIVO/Tienda/Tienda_02_Carrito/script.js
+++
@@ -1,9 +0,0 @@
-function addToCart(name, price, image) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-  cart.push({ name, price, image });
-
-  localStorage.setItem("cart", JSON.stringify(cart));
-
-  alert(name + " ha sido agregado al carrito 🛒");
-}
